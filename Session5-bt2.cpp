cfe5807a49a73a82c35d0d0ab0e509e617c3fbf7
huylg <lamgiahuy002203@gmail.com>
2024-12-16 09:21:09 +0700

    Hoàn thành bt2

---
+++ Session5-bt2.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("================== TRO CHOI DOAN SO DUNG PHAM VI TU 1-100 =================="); 
+	int n;
+	int x = 45; 
+	do{
+		printf("\nMoi doan so: ");
+		scanf("%d",&n); 
+		 if(n != x){
+		printf("So nhap vao khac voi dap an roi");
+		}
+		}while(n != x) ;
+			printf("So nhap vao hoan toan chinh xac voi dap an "); 
+		
+	} 
+
